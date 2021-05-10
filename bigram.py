01d9550f9b3bd482ab4f06b079b46c2fc5ef6c38
remy-z <remyszach@gmail.com>
2021-05-09 21:22:15 -0700

    Added the skeleton programs

---
+++ bigram.py
@@ -0,0 +1,14 @@
+# !/usr/bin/python3
+
+
+# TODO: Implement a Laplace-smoothed bigram model :)
+class LanguageModel:
+
+    def __init__(self):
+        pass
+
+    def train(self, train_corpus):
+        print('I am an unimplemented BIGRAM train() method.')  # delete this!
+
+    def score(self, test_corpus):
+        print('I am an unimplemented BIGRAM score() method.')  # delete this!
